314eb2ed51de7dd9397282c3b77d4ba4b966b186
reanqbow <reanqbow@gmail.com>
2021-11-15 20:27:49 -0500

    change parse agent

--- SumoGH/SumoGH/ParseAgent.cs
+++ SumoGH/SumoGH/ParseAgent.cs
@@ -20,30 +20,55 @@ namespace SumoGH
             return i;
         }
 
+        public static Agent MakeVeh(XElement veh, string id, int counter)
+        {
+            string type = veh.Name.ToString();
+            double x = Convert.ToDouble(veh.Attribute("x").Value);
+            double y = Convert.ToDouble(veh.Attribute("y").Value);
+            double degree = Convert.ToDouble(veh.Attribute("angle").Value);
+            string laneId = veh.Attribute("lane").Value;
+
+            Dictionary<int, Tuple<double, double, double, string>> dict =
+               new Dictionary<int, Tuple<double, double, double, string>>();
+
+            dict.Add(counter, Tuple.Create(x, y, degree, laneId));
+            Agent a = new Agent(type, id, dict);
+
+            return a;
+        }
+
+        public static Agent MakePed(XElement veh, string id, int counter)
+        {
+            string type = veh.Name.ToString();
+            double x = Convert.ToDouble(veh.Attribute("x").Value);
+            double y = Convert.ToDouble(veh.Attribute("y").Value);
+            double degree = Convert.ToDouble(veh.Attribute("angle").Value);
+            string laneId = veh.Attribute("edge").Value;
+
+            Dictionary<int, Tuple<double, double, double, string>> dict =
+               new Dictionary<int, Tuple<double, double, double, string>>();
+
+            dict.Add(counter, Tuple.Create(x, y, degree, laneId));
+            Agent a = new Agent(type, id, dict);
+
+            return a;
+        }
+
         public Dictionary<string, Agent> Parse(IEnumerable<XElement> xe)
         {
             Dictionary<string, Agent> res = new Dictionary<string, Agent>();
             int counter = 0;
             foreach (XElement timestep in xe)
             {
-
                 IEnumerable<XElement> de = timestep.Descendants();
                 foreach (XElement v in de)
                 {
+                    string id = v.Attribute("id").Value;
                     if (v.Name.ToString() != "person" && v.Name.ToString() != "bike")
                     {
-                        string id = v.Attribute("id").Value;
                         if (!res.ContainsKey(id))
                         {
-                            string type = v.Name.ToString();
-                            double x = Convert.ToDouble(v.Attribute("x").Value);
-                            double y = Convert.ToDouble(v.Attribute("y").Value);
-                            double degree = Convert.ToDouble(v.Attribute("angle").Value);
-                            string laneId = v.Attribute("lane").Value;
-                            Dictionary<int, Tuple<double, double, double, string>> dict =
-                               new Dictionary<int, Tuple<double, double, double, string>>();
-                            dict.Add(counter, Tuple.Create(x, y, degree, laneId));
-                            Agent a = new Agent(type, id, dict);
+                            Agent a = MakeVeh(v, id, counter);
                             res.Add(id, a);
                         }
                         else
@@ -57,18 +82,9 @@ namespace SumoGH
                     }
                     else
                     {
-                        string id = v.Attribute("id").Value;
                         if (!res.ContainsKey(id))
                         {
-                            string type = v.Name.ToString();
-                            double x = Convert.ToDouble(v.Attribute("x").Value);
-                            double y = Convert.ToDouble(v.Attribute("y").Value);
-                            double degree = Convert.ToDouble(v.Attribute("angle").Value);
-                            string laneId = v.Attribute("edge").Value;
-                            Dictionary<int, Tuple<double, double, double, string>> dict =
-                               new Dictionary<int, Tuple<double, double, double, string>>();
-                            dict.Add(counter, Tuple.Create(x, y, degree, laneId));
-                            Agent a = new Agent(type, id, dict);
+                            Agent a = MakePed(v, id, counter);
                             res.Add(id, a);
                         }
                         else
